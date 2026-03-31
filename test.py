d78285023ef277f52d0e99f277c810ac1f78c915
Gabriel <gagames08@gmail.com>
2026-03-31 11:57:04 -0300

    Updated .gitignore

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 from test2 import nome
 
 print(nome)
+
+# git --no-pager: print the output normally in the terminal, without the program;
+# git log --oneline: prints a summed up version of the git history
+# git remote add/rm name: Add/removes a remote repository;
+# git push name branch -u: makes the repository the standard repository; -> can push with only git push

49d53a0522e02e6e8fde5a47538d74b506ee8e50
Gabriel <gabriel.alboneti@gmail.com>
2026-03-31 17:04:15 -0300

    Added ruff.toml file

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from test2 import nome
 
+
+def testing_linter(x, y):
+   return x + y
+
+
 print(nome)
 
 # git status: check the status of the current changes;
@@ -7,6 +12,7 @@ print(nome)
 # git --no-pager: print the output normally in the terminal, without the program;
 # git log --oneline: prints a summed up version of the git history
 # git remote add/rm name: Add/removes a remote repository;
-# git push name branch -u: makes the repository the standard repository; -> can push with only git push
+# git push name branch -u: makes the repository the standard repository;
+#     - can push with only git push
 # ssh-keygen: generates ssh keys to use with github
 # git config: used to configures things like username and email;

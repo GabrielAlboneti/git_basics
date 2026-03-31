c9734c1358408181ef3eb0643ffc339c3887646c
Gabriel <gabriel.alboneti@gmail.com>
2026-03-31 12:20:08 -0300

    Added more comments to test.py

--- test.py
+++ test.py
@@ -2,7 +2,11 @@ from test2 import nome
 
 print(nome)
 
+# git status: check the status of the current changes;
+# git add .: adds all changes to the commit;
 # git --no-pager: print the output normally in the terminal, without the program;
 # git log --oneline: prints a summed up version of the git history
 # git remote add/rm name: Add/removes a remote repository;
 # git push name branch -u: makes the repository the standard repository; -> can push with only git push
+# ssh-keygen: generates ssh keys to use with github
+# git config: used to configures things like username and email;
